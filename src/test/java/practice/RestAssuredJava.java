6820d6a78f41deb9c2380b93f656ef7ba17a1d7b
raheela <raheelab845@gmail.com>
2025-06-12 14:30:38 +0530

    client B commit

--- src/test/java/practice/RestAssuredJava.java
+++ src/test/java/practice/RestAssuredJava.java
@@ -4,6 +4,14 @@ import org.testng.annotations.Test;
 
 public class RestAssuredJava {
 	
+	@Test
+	public void restAssuredTest1() {
+		System.out.println("restAssuredTest1");
+	}
 	
+	@Test
+	public void restAssuredTest2() {
+		System.out.println("restAssuredTest2");
+	}
 
 }
