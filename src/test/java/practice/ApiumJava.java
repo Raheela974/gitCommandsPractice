a3ff2153d417e73e74475343b0446de522eff1e6
raheela <raheelab845@gmail.com>
2025-06-12 12:34:54 +0530

    first commit

---
+++ src/test/java/practice/ApiumJava.java
@@ -0,0 +1,17 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class ApiumJava {
+	
+	@Test
+	public void apiumJavaTest1() {
+		System.out.println("apiumJavaTest1");
+	}
+	
+	@Test
+	public void apiumJavaTest2() {
+		System.out.println("apiumJavaTest2");
+	}
+
+}
---
+++ src/test/java/practice/SeleniumJava.java
@@ -0,0 +1,16 @@
+package practice;
+
+import org.testng.annotations.Test;
+
+public class SeleniumJava {
+
+	@Test
+	public void seleniumTest1() {
+		System.out.println("seleniumTest1");
+	}
+	
+	@Test
+	public void seleniumTest2() {
+		System.out.println("seleniumTest2");
+	}
+}
